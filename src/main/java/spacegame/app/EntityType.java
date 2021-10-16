041368a058dbe147e7c25824db683ec344312433
Dakota Sliter <djsliter@gmail.com>
2021-10-16 18:05:06 -0400

    Commented code and added Javadocs to every class and method. Additionally, tweaked spaceship speed settings.

--- src/main/java/spacegame/app/EntityType.java
+++ src/main/java/spacegame/app/EntityType.java
@@ -1,5 +1,8 @@
 package spacegame.app;
 
+/**
+ * Enum class defining physical game object types
+ */
 public enum EntityType { //lays out all physical game object types
     PLAYER, PROJECTILE, DEBRIS
 }
