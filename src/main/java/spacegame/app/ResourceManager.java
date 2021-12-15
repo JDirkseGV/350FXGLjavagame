c294d067416f4a65d0f05a1fc96da68c9ade2c0d
Dakota Sliter <djsliter@gmail.com>
2021-12-14 21:02:22 -0500

    Added comments to ResourceManager

--- src/main/java/spacegame/app/ResourceManager.java
+++ src/main/java/spacegame/app/ResourceManager.java
@@ -6,15 +6,31 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class ResourceManager {
-
+    /**
+     * Handles saving game score and loading
+     * @param data
+     * @param fileName
+     * @throws Exception
+     */
     public static void save(Serializable data, String fileName) throws Exception {
+        /*
+          Saves player data
+         */
+
+        // try to create ObjectOutputStream
         try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(Paths.get(fileName)))) {
+            // Write save data to file
             oos.writeObject(data);
         }
     }
 
     public static Object load(String fileName) throws Exception {
+        /*
+          Loads player data from file
+         */
+        // Attempts to open save data
         try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(Paths.get(fileName)))) {
+            // Loads save data
             return ois.readObject();
         }
     }
